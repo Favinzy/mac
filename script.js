96f3c761ade4ebfb5b7309a8f0e1542df5bff58e
Favinzy <mmesomaoguezu@gmail.com>
2024-10-02 05:39:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let media = document.querySelector(".media");
+
+function slide() {
+  media.classList.toggle("responsive");
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let slidingText = document.getElementById("slidingText");
+//   setTimeout(function () {
+//     slidingText.classList.add("show");
+//   }, 500);
+// });
